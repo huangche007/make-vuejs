e055a8cac1fb7d17d7e3d75a0fb14ba5ee12c867
huangche007 <hc386271623@163.com>
2019-10-07 17:27:06 +0800

    指令解析模块：将属性解析成指令(v-、@click、:show="isShow")等

--- src/parser.js
+++ src/parser.js
@@ -1,4 +1,11 @@
 import {assert} from './utils'
+/**
+ *
+ *将html元素用js对象进行表示
+ * @export
+ * @param {*} dom
+ * @returns
+ */
 export function parseDom(dom){
     assert(dom,`the element ${dom} is required`);
     assert(dom instanceof Node,`the type of ${dom} is Node`);
@@ -35,4 +42,50 @@ export function parseDom(dom){
         }   
        
     }
+}
+/**
+ *
+ *根据属性解析指令
+ * @export
+ * @param {*} attrs
+ * @returns
+ */
+export function parseDirectives(attrs){
+    assert(attrs,`${attrs} is not found`);
+    assert(attrs.constructor === Object,`${attrs} is not an object`);
+    console.log(attrs);
+    let directives = [];
+    for(let key in attrs){
+        let directive = {};
+        if(key.startsWith('v-')){ //v-bind:name="banner" v-bind:title="这是banner" v-on:click="xxs" v-if="isShow"
+            // 名称：参数
+            let [name,arg] = key.split(":");
+
+            directive = {
+                name:name.replace(/^v\-/,''),
+                arg
+            }
+        }else if(key.startsWith(':')){ //:showbanner: "showBanner"
+            directive = {
+                name:'bind',
+                arg:key.substring(1)
+            }
+        }else if(key.startsWith('@')){ //@click: "func(12)"
+            directive = {
+                name:'on',
+                arg:key.substring(1)
+            }
+        }
+        if(directive){
+            //:="666" v-bind="888"
+            assert(directive.name==="bind" && directive.arg || directive.name!=="bind",`the directive ${key} is invalidate`);
+            //v-on="fck"
+            assert(directive.name==="on" && directive.arg || directive.name!=="on",`the event ${key} is invalidate`);
+            directive.value = attrs[key];
+            directives.push(directive);
+        }
+       
+    }
+    return directives;
+
 }
