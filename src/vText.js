930a84496d8a27e37e8b00b43a8f09146fa12f79
huangche007 <hc386271623@163.com>
2019-10-08 11:00:02 +0800

    创建虚拟文本节点对象

--- src/vText.js
+++ src/vText.js
@@ -1,7 +1,12 @@
 
 import HCNode from './hcNode'
+import {assert} from './utils';
 export default class VText extends HCNode{
-    constructor(){
-        super();
+    constructor(options){
+        assert(options);
+        assert(options.el);
+        assert(options.data);
+        super(options.el);
+        this._data = options.data;
     }
 }
