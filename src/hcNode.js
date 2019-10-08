dd0f0bd5f3c3d3bd09080d56b58b1496239782bb
huangche007 <hc386271623@163.com>
2019-10-08 11:02:20 +0800

    创建虚拟DOM父类

--- src/hcNode.js
+++ src/hcNode.js
@@ -1,6 +1,9 @@
 
+import {assert} from './utils';
 export default class HCNode{
-    constructor(){
-        
+    constructor(el){
+        assert(el,`${el} is not found`);
+        assert(el instanceof Node,`the type of ${el} is a Node`);
+        this._el = el;
     }
 }
