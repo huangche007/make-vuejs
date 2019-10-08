f302e06065d13ee2deed4940607d442e0097e501
huangche007 <hc386271623@163.com>
2019-10-08 23:43:32 +0800

    文本虚拟节点：字符串模板解析功能

--- src/vText.js
+++ src/vText.js
@@ -1,12 +1,22 @@
 
 import HCNode from './hcNode'
 import {assert} from './utils';
+import {complieStringTemplate} from './experssion';
 export default class VText extends HCNode{
-    constructor(options){
+    constructor(options,cmp){
         assert(options);
         assert(options.el);
         assert(options.data);
-        super(options.el);
-        this._data = options.data;
+        super(options.el,cmp);
+        // console.log('options:',options);
+        this._strTemplate = options.data;
+        this.status = 'init';
+
+    }
+    render(){
+        // console.log('this._component:',this._component);
+        const strExp = complieStringTemplate(this._strTemplate,this._component.data);
+        this._el.data = strExp;
+        this.status = 'update';
     }
 }
