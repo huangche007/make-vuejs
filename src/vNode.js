a5ce70d2fd37e898555208a8fda7938bf0bd5044
huangche007 <hc386271623@163.com>
2019-10-08 11:00:55 +0800

    创建虚拟Html元素节点虚拟DOM

--- src/vNode.js
+++ src/vNode.js
@@ -1,7 +1,19 @@
 
 import HCNode from './hcNode'
+import {assert} from './utils';
+import {parseDirectives,parseEvent} from './parser';
 export default class VNode extends HCNode{
-    constructor(){
-        super();
+    constructor(options){
+        assert(options,`${options} is not found`);
+        assert(options.el,`${options.el} is not found`);
+        assert(options.tag,`${options.tag} is not found`);
+        assert(options.attrs,`${options.attrs} is not found`);
+        assert(options.children,`${options.children} is not found`);
+        super(options.el);
+
+        this.type = options.tag;
+        this.$attrs = options.attrs;
+        this.$directives = parseDirectives(this.$attrs);
+        this.$listener = parseEvent(this.$directives);
     }
 }
