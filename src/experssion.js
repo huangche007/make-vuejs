8820b904597900b098cac31a57ce0a23ac9c30c4
huangche007 <hc386271623@163.com>
2019-10-08 23:42:47 +0800

    新增字符串解析模块，如{{a+b}}

--- src/experssion.js
+++ src/experssion.js
@@ -7,21 +7,20 @@ const globalKey={
 }
 export function parseExperssion(str,data){
     const arr = getExperssion(str);
-    console.log('arr:',arr);
     const filterArr = arr.map((st) => {
         if(typeof st === 'string'){
-            return `'${st}'`;
+            return "'"+st+"'";
         }else{
             const mStr = st.express.replace(/.?[\$_a-z][a-z0-9_\$]*/ig,(s) =>{
                 if(/[\$_a-z]/i.test(s[0])){
-                   return dealGlobal(data,s,`data.${s}`);
-                    // return `data.${s}`;
+                //    return dealGlobal(data,s,`data.${s}`);
+                    return "data."+s;
                 }else{
                     if(s[0] === '.'){
                         return s;
                     }else{
-                        return s[0]+dealGlobal(data,s.substring(1),`data.${s.substring(1)}`);
-                        // return `${s[0]}data.${s.substring(1)}`;
+                        // return s[0]+dealGlobal(data,s.substring(1),`data.${s.substring(1)}`);
+                        return s[0]+"data."+s.substring(1);
                     }
                 }
 
@@ -29,10 +28,51 @@ export function parseExperssion(str,data){
             return mStr;
         }
     })
+
     const filterStr = filterArr.join('');
-    console.log('arr:',filterStr,eval(filterStr));
-    eval(filterStr);
+
+   return eval(filterStr);
 }
+/**
+ *
+ *解析字符串模板
+ * @export
+ * @param {*} str
+ * @param {*} data
+ * @returns
+ */
+export function complieStringTemplate(str,data){
+    let start = 0;
+    let arr = [];
+    let n = 0;
+    while((n=str.indexOf("{{",start))!==-1){
+        arr.push(str.substring(start,n));
+
+        let m = 2;
+        let end;
+        for(let i = n+2;i<str.length;i++){
+            if(str[i] === "{") m++;
+            else if(str[i] === "}") m--;
+
+            if(m ===0){
+                end = i;
+                break;
+            }
+        }
+
+        if(m>0){
+            assert(false,`{}没有配对`);
+        }
+        // {{a}}
+        const mStr = str.substring(n+2,end-1);
+        arr.push(parseExperssion(mStr,data));
+        start = end+1;
+    }
+    arr.push(str.substring(start));
+    return arr.join("");
+}
+
+
 /**
  *兼容处理内置的一些表达式，如Math.min(12,14)、parseFloat(x)等
  * @param {*} data
